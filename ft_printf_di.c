28a063bcc0e3bad85c9158ae7a2594df92a86914
Yamada_Ika <yamadaiori@yamadaiorinoMacBook-Air.local>
2021-12-06 01:57:30 +0900

    Add NULL validation

--- ft_printf_di.c
+++ ft_printf_di.c
@@ -6,7 +6,7 @@
 /*   By: iyamada <iyamada@student.42tokyo.jp>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/11/14 14:06:58 by iyamada           #+#    #+#             */
-/*   Updated: 2021/11/15 04:06:56 by iyamada          ###   ########.fr       */
+/*   Updated: 2021/11/19 11:55:55 by iyamada          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,6 +17,11 @@ void	ft_printf_di(va_list *ap, int *write_len)
 	char	*num;
 
 	num = ft_itoa(va_arg(*ap, int));
+	if (num == NULL)
+	{
+		*write_len = -1;
+		return ;
+	}
 	ft_putstr(num);
 	*write_len += (int)ft_strlen_s(num);
 	ft_free_s((void **)&num);
--- ft_printf_u.c
+++ ft_printf_u.c
@@ -6,7 +6,7 @@
 /*   By: iyamada <iyamada@student.42tokyo.jp>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/11/14 14:07:47 by iyamada           #+#    #+#             */
-/*   Updated: 2021/11/14 15:31:50 by iyamada          ###   ########.fr       */
+/*   Updated: 2021/11/16 23:56:33 by iyamada          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,6 +17,11 @@ void	ft_printf_u(va_list *ap, int *write_len)
 	char	*num;
 
 	num = ft_uitoa(va_arg(*ap, unsigned int));
+	if (num == NULL)
+	{
+		*write_len = -1;
+		return ;
+	}
 	ft_putstr(num);
 	*write_len += (int)ft_strlen_s(num);
 	ft_free_s((void **)&num);
