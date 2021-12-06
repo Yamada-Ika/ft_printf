5539a70bde17d1d2e2868b134dcf0be1ca158d10
Yamada_Ika <yamadaiori@yamadaiorinoMacBook-Air.local>
2021-12-06 11:48:21 +0900

    Remove malloc ft_printf_helper

--- ft_printf.c
+++ ft_printf.c
@@ -6,82 +6,59 @@
 /*   By: iyamada <iyamada@student.42tokyo.jp>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/10/25 14:06:36 by iyamada           #+#    #+#             */
-/*   Updated: 2021/11/15 02:43:48 by iyamada          ###   ########.fr       */
+/*   Updated: 2021/12/06 02:34:55 by iyamada          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "ft_printf.h"
 
-static void	ft_printf_non_converted_str(const char *format,
-	size_t head, size_t tail, int *write_len)
-{
-	char	*str;
-
-	if (*write_len < 0)
-		return ;
-	str = ft_substr(format, head, tail - head);
-	if (str == NULL)
-	{
-		*write_len = -1;
-		return ;
-	}
-	ft_putstr(str);
-	*write_len += ft_strlen_s(str);
-	ft_free_s((void **)&str);
-}
-
 static void	ft_printf_converted_str(const char *format,
-	va_list *ap, size_t tail, int *write_len)
+	va_list *ap, size_t index, int *write_len)
 {
-	size_t	i;
-
 	if (*write_len < 0)
 		return ;
-	i = tail + 1;
-	if (format[i] == 'c')
+	index++;
+	if (format[index] == 'c')
 		ft_printf_c(ap, write_len);
-	else if (format[i] == 's')
+	else if (format[index] == 's')
 		ft_printf_s(ap, write_len);
-	else if (format[i] == 'p')
+	else if (format[index] == 'p')
 		ft_printf_p(ap, write_len);
-	else if (format[i] == 'd' || format[i] == 'i')
+	else if (format[index] == 'd' || format[index] == 'i')
 		ft_printf_di(ap, write_len);
-	else if (format[i] == 'u')
+	else if (format[index] == 'u')
 		ft_printf_u(ap, write_len);
-	else if (format[i] == 'x')
+	else if (format[index] == 'x')
 		ft_printf_lower_x(ap, write_len);
-	else if (format[i] == 'X')
+	else if (format[index] == 'X')
 		ft_printf_upper_x(ap, write_len);
-	else if (format[i] == '%')
+	else if (format[index] == '%')
 		ft_printf_percent(write_len);
 }
 
 static int	ft_printf_helper(const char *format, va_list *ap)
 {
 	int		write_len;
-	size_t	tail;
-	size_t	head;
+	size_t	i;
 
 	write_len = 0;
-	tail = 0;
-	head = 0;
-	while (format[tail] != '\0')
+	i = 0;
+	while (format[i] != '\0')
 	{
-		if (format[tail] == '%')
+		if (format[i] == '%')
 		{
-			ft_printf_non_converted_str(format, head, tail, &write_len);
+			ft_printf_converted_str(format, ap, i, &write_len);
 			if (write_len == -1)
 				return (write_len);
-			ft_printf_converted_str(format, ap, tail, &write_len);
-			if (write_len == -1)
-				return (write_len);
-			tail += 2;
-			head = tail;
+			i++;
 		}
 		else
-			tail++;
+		{
+			ft_putchar(format[i]);
+			write_len++;
+		}
+		i++;
 	}
-	ft_printf_non_converted_str(format, head, tail, &write_len);
 	return (write_len);
 }
 
