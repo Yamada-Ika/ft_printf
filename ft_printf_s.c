dd7310da399c975f94f380d5baaa05380c2aea26
Yamada-Ika <73375669+Yamada-Ika@users.noreply.github.com>
2021-12-07 02:42:49 +0900

    [Fix] do not display string in case of big string

--- ft_printf_s.c
+++ ft_printf_s.c
@@ -6,7 +6,7 @@
 /*   By: iyamada <iyamada@student.42tokyo.jp>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/11/14 14:04:55 by iyamada           #+#    #+#             */
-/*   Updated: 2021/12/07 02:39:03 by iyamada          ###   ########.fr       */
+/*   Updated: 2021/12/07 02:40:38 by iyamada          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,15 +15,20 @@
 size_t	ft_printf_s(va_list *ap, size_t write_len)
 {
 	char	*str;
+	size_t	str_len;
 
 	str = va_arg(*ap, char *);
+	
 	if (str == NULL)
 	{
 		ft_putstr("(null)");
 		write_len += ft_strlen_s("(null)");
 		return (write_len);
 	}
+	str_len = ft_strlen_s(str);
+	if (str_len > INT_MAX)
+		return (ERROR);
 	ft_putstr(str);
-	write_len += ft_strlen_s(str);
+	write_len += str_len;
 	return (write_len);
 }
