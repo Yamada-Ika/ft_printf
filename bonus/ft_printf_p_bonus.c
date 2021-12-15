790bfb1ab01289508e32ea5b2e45912f8d3f95b5
Yamada-Ika <73375669+Yamada-Ika@users.noreply.github.com>
2021-12-15 16:52:44 +0900

    [Fix]rename variable name

--- bonus/ft_printf_p_bonus.c
+++ bonus/ft_printf_p_bonus.c
@@ -6,7 +6,7 @@
 /*   By: iyamada <iyamada@student.42tokyo.jp>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/11/14 14:05:27 by iyamada           #+#    #+#             */
-/*   Updated: 2021/12/10 17:14:33 by iyamada          ###   ########.fr       */
+/*   Updated: 2021/12/15 16:52:26 by iyamada          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,12 +14,12 @@
 
 size_t	ft_printf_p(va_list *ap, size_t write_len, t_flag_manager *flags)
 {
-	char	*hex;
+	char	*hex_str;
 
-	hex = ft_ultoa_base((unsigned long)va_arg(*ap, void *), "0123456789abcdef");
-	if (hex == NULL)
+	hex_str = ft_ultoa_base((unsigned long)va_arg(*ap, void *), "0123456789abcdef");
+	if (hex_str == NULL)
 		return (ERROR);
-	write_len = ft_print_with_flags(flags, &hex, write_len);
-	ft_free_s((void **)&hex);
+	write_len = ft_print_with_flags(flags, &hex_str, write_len);
+	ft_free_s((void **)&hex_str);
 	return (write_len);
 }
